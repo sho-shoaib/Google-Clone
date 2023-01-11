138018436b813744695e757467557c3e692e0109
Shoaib Shaikh <shaikhsho292@gmail.com>
2023-01-11 22:13:41 +0530

    header

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import User from "./User";
+
+const Header = () => {
+  return (
+    <header className='flex justify-between items-center p-4 [&>*]:text-sm'>
+      <div className='flex gap-6'>
+        <p className='link'>About</p>
+        <p className='link'>Store</p>
+      </div>
+      <div className='flex items-center gap-6'>
+        <p className='link'>Gmail</p>
+        <p className='link'>Images</p>
+        <User />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/User.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = () => {
+  return <div className='bg-neutral-600 rounded-full h-8 w-8'></div>;
+};
+
+export default User;
