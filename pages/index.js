9c154cbc0d7cf1b1eb8067fed7ca25e852d6588c
Shoaib Shaikh <shaikhsho292@gmail.com>
2023-01-12 15:57:29 +0530

    body frontend

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import { getProviders } from "next-auth/react";
 import Head from "next/head";
+import Image from "next/image";
 import Header from "../components/Header";
+import googleLogo from "../assets/google-logo.png";
+import { AiOutlineSearch } from "react-icons/ai";
+import { GrClose } from "react-icons/gr";
+import { useState } from "react";
+import { useRef } from "react";
 
 export default function Home({ providers }) {
+  const [inputActive, setInputActive] = useState(false);
+  const inputRef = useRef(null);
+
   return (
     <>
       <Head>
@@ -12,6 +21,36 @@ export default function Home({ providers }) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header providers={providers} />
+      <form
+        action=''
+        className='w-full max-w-xl flex flex-col items-center mx-auto px-4'
+      >
+        <Image src={googleLogo} width={270} />
+        <div
+          className={`flex items-center gap-3 rounded-full pl-3.5 border-2 w-full -mt-6 hover:shadow-md overflow-hidden ${
+            inputActive && "shadow-md"
+          }`}
+          onClick={() => inputRef.current.focus()}
+        >
+          <AiOutlineSearch size={22} className='text-neutral-500' />
+          <input
+            ref={inputRef}
+            onFocus={() => setInputActive(true)}
+            onBlur={() => setInputActive(false)}
+            type='text'
+            className='w-full py-3 text-sm outline-none'
+          />
+          <GrClose
+            onClick={() => (inputRef.current.value = "")}
+            size={49}
+            className='text-neutral-100 opacity-60 py-4 pr-3.5 '
+          />
+        </div>
+        <div className='flex gap-4 mt-6'>
+          <button className='home-btn'>Google Search</button>
+          <button className='home-btn'>I'm Feeling Lucky</button>
+        </div>
+      </form>
     </>
   );
 }
