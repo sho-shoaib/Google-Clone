428bdc882270abe3917cab2f5e3ee7e3f4fc9245
Shoaib Shaikh <shaikhsho292@gmail.com>
2023-01-19 17:27:55 +0530

    added image result component

--- pages/index.js
+++ pages/index.js
@@ -32,49 +32,50 @@ export default function Home({ providers }) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Header providers={providers} />
+      <div className='relative h-screen'>
+        <Header providers={providers} />
 
-      <main>
-        <form
-          onSubmit={search}
-          className='w-full max-w-xl flex flex-col items-center mx-auto px-4'
-        >
-          <Image
-            title='Google'
-            className='px-10'
-            src={googleLogo}
-            width={350}
-          />
-          <div
-            className={`flex items-center gap-3 rounded-full pl-3.5 border-2 w-full -mt-6 hover:shadow-md overflow-hidden ${
-              inputActive && "shadow-md"
-            }`}
-            onClick={() => inputRef.current.focus()}
+        <main>
+          <form
+            onSubmit={search}
+            className='w-full max-w-xl flex flex-col items-center mx-auto px-4'
           >
-            <AiOutlineSearch size={22} className='text-neutral-500' />
-            <input
-              ref={inputRef}
-              onFocus={() => setInputActive(true)}
-              onBlur={() => setInputActive(false)}
-              type='text'
-              className='w-full py-3 text-sm outline-none'
+            <Image
+              title='Google'
+              className='px-10'
+              src={googleLogo}
+              width={350}
             />
-            <GrClose
-              onClick={() => (inputRef.current.value = "")}
-              size={49}
-              className='text-neutral-100 opacity-60 py-4 pr-3.5 '
-            />
-          </div>
-          <div className='flex gap-4 mt-6'>
-            <button type='submit' className='home-btn'>
-              Google Search
-            </button>
-            <button className='home-btn'>I'm Feeling Lucky</button>
-          </div>
-        </form>
-      </main>
-
-      <Footer />
+            <div
+              className={`flex items-center gap-3 rounded-full pl-3.5 border-2 w-full -mt-6 hover:shadow-md overflow-hidden ${
+                inputActive && "shadow-md"
+              }`}
+              onClick={() => inputRef.current.focus()}
+            >
+              <AiOutlineSearch size={22} className='text-neutral-500' />
+              <input
+                ref={inputRef}
+                onFocus={() => setInputActive(true)}
+                onBlur={() => setInputActive(false)}
+                type='text'
+                className='w-full py-3 text-sm outline-none'
+              />
+              <GrClose
+                onClick={() => (inputRef.current.value = "")}
+                size={49}
+                className='text-neutral-100 opacity-60 py-4 pr-3.5 '
+              />
+            </div>
+            <div className='flex gap-4 mt-6'>
+              <button type='submit' className='home-btn'>
+                Google Search
+              </button>
+              <button className='home-btn'>I'm Feeling Lucky</button>
+            </div>
+          </form>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
--- pages/search/index.js
+++ pages/search/index.js
@@ -6,11 +6,16 @@ import { getProviders } from "next-auth/react";
 import { allResults, imageResults } from "../../Response";
 import parse from "html-react-parser";
 import SearchPagination from "../../components/SearchPagination";
+import Image from "next/image";
+import Link from "next/link";
+import Footer from "../../components/Footer";
 
 const index = ({ providers, results }) => {
   const router = useRouter();
-  const { term } = router.query;
-  console.log(results);
+  const { term, searchType } = router.query;
+  const myLoader = ({ src }) => {
+    return src;
+  };
 
   return (
     <>
@@ -20,35 +25,81 @@ const index = ({ providers, results }) => {
 
       <SearchHeader providers={providers} />
 
-      <main className='w-full xl:max-w-full max-w-6xl xl:px-48 py-2 md:px-11 px-4 mx-auto'>
-        <p className='text-sm opacity-80 mb-5'>
-          About {results.searchInformation.formattedTotalResults} results (
-          {results.searchInformation.formattedSearchTime} seconds)
-        </p>
-        <div className='flex flex-col gap-8 max-w-2xl'>
-          {results.items.map((item) => {
-            return (
-              <div className='flex flex-col items-start gap-1'>
-                <a
-                  href={item.formattedUrl}
-                  className='flex flex-col max-w-xl group gap-1'
+      {searchType ? (
+        <>
+          <main className='p-5 w-full flex flex-wrap gap-6'>
+            {results.items.map((item) => {
+              const {
+                title,
+                displayLink,
+                link,
+                image: { contextLink },
+              } = item;
+              return (
+                <Link
+                  href={contextLink}
+                  style={{ height: "180px" }}
+                  className='grow mb-10 w-max group'
                 >
-                  <p className='text-sm truncate text-neutral-800'>
-                    {item.formattedUrl}
-                  </p>
-                  <h2 className='text-blue-800 text-xl group-hover:underline '>
-                    {item.title}
-                  </h2>
-                </a>
-                <p className='text-sm text-neutral-700'>
-                  {parse(item.htmlSnippet)}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-        <SearchPagination />
-      </main>
+                  <div className='h-full rounded-xl overflow-hidden bg-neutral-100 flex justify-center group-hover:shadow-lg'>
+                    <img
+                      src={link}
+                      alt='image'
+                      className='max-w-full max-h-full object-cover align-bottom'
+                    />
+                  </div>
+                  <div className='w-full max-w-xs mt-0.5'>
+                    <p className='truncate text-sm opacity-80 mb-0.5 group-hover:underline group-hover:underline-offset-2'>
+                      {displayLink}
+                    </p>
+                    <h2 className='truncate text-sm opacity-95 group-hover:underline group-hover:underline-offset-2'>
+                      {title}
+                    </h2>
+                  </div>
+                </Link>
+              );
+            })}
+          </main>
+          <div className='md:-ml-48 xs:ml-0'>
+            <SearchPagination />
+          </div>
+        </>
+      ) : (
+        <>
+          <main className='w-full xl:max-w-full max-w-6xl xl:px-48 py-2 md:px-11 px-4 mx-auto'>
+            <p className='text-sm opacity-80 mb-5'>
+              About {results.searchInformation.formattedTotalResults} results (
+              {results.searchInformation.formattedSearchTime} seconds)
+            </p>
+            <div className='flex flex-col gap-8 max-w-2xl'>
+              {results.items.map((item) => {
+                return (
+                  <div className='flex flex-col items-start gap-1'>
+                    <a
+                      href={item.formattedUrl}
+                      className='flex flex-col max-w-xl group gap-1'
+                    >
+                      <p className='text-sm truncate text-neutral-800'>
+                        {item.formattedUrl}
+                      </p>
+                      <h2 className='text-blue-800 text-xl group-hover:underline '>
+                        {item.title}
+                      </h2>
+                    </a>
+                    <p className='text-sm text-neutral-700'>
+                      {parse(item.htmlSnippet)}
+                    </p>
+                  </div>
+                );
+              })}
+            </div>
+            <SearchPagination />
+          </main>
+          <div className='mt-12'>
+            <Footer />
+          </div>
+        </>
+      )}
     </>
   );
 };
